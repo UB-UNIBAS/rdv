e0b11dbabc3747984c1191c8c068be9b48c5209b
Sebastian Schüpbach <sebastian.schuepbach@unibas.ch>
2019-07-04 12:36:41 +0200

    fix small lint error
    Signed-off-by: Sebastian Schüpbach <sebastian.schuepbach@unibas.ch>

--- src/app/shared/services/basket-request-interceptor.service.ts
+++ src/app/shared/services/basket-request-interceptor.service.ts
@@ -47,7 +47,7 @@ export class BasketRequestInterceptor implements HttpInterceptor {
               return event;
             }
           }
-        );
+        ));
       } else {
         return next.handle(req);
       }
